53106d9f0b7b00aee25626ca874493ae65900291
Sam Moses <sammoses992@gmail.com>
2026-05-14 10:08:47 +0100

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import sys
 
 import pandas as pd
 import numpy as np  
-
+from sklearn.metrics import r2_score
 
 from src.logger import logging
 from src.exception import CustomException
@@ -22,4 +22,28 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
 
     except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+
+    try:
+        report = {}
+
+        for i in range(len(models)):
+
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)  # Train the model
+
+            y_train_pred = model.predict(X_train)  # Predict on the training set
+            y_test_pred = model.predict(X_test)  # Predict on the test set
+
+            test_model_score = r2_score(y_test, y_test_pred)  # Calculate R2 score
+
+            report[list(models.keys())[i]] = test_model_score  # Store the score in the report
+
+        return report
+
+    except Exception as e:
+        logging.error("Error occurred during model evaluation.")
         raise CustomException(e, sys)
